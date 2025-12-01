2c1384f7a138bb54e43eaf038333e14bceaaa97c
Stavish <ns38770@email.vccs.edu>
2025-12-01 09:55:27 -0500

    Work on curses logic

--- FractionUI.cpp
+++ FractionUI.cpp
@@ -1,3 +1,4 @@
+#include "Fraction.h"
 #include <ncurses.h>
 #include <iostream>
 #include <string>
@@ -8,22 +9,51 @@ int main(){
     
     int ymax, xmax;
  
-    int numerator;
-    int denominator;
+    int num1;
+    int den1;
     char operateur; //bc operator is a keyword lol
     
-    int newnum;
-    int newden;
-
+    int num2;
+    int den2;
+    
+    int resultnum;
+    int resultdenom;
+    
+    // activated after pressing enter on each value to know when to make objects
+    // bool num1_done = false;
+    // bool den1_done = false;
+    // bool operator_done = false;
+    
+    // bool num2_done = false;
+    // bool den2_done = false;
 
     initscr();
-    noecho();
+    // title
+    move(0,(xmax/2));
+    printw("Welcome to the nCurses Calculator!");
+    refresh();
+    
+    echo();
     cbreak();
     getmaxyx(stdscr, ymax, xmax);
+    
+    // y, x
+    move(1, 0);
+    scanw("%d", &num1);
 
-    move(1, 1);
+    move(3, 0);
+    scanw("%d", &den1);
     
+
+    move(ymax / 2, xmax / 2);
+
+    scanw("%c", &operateur);
+
+    move (0, xmax - 1);
+    scanw("%d", &num2);
     
+    move (3, xmax - 1);
+    scanw("%d", &den2);
 
     endwin();
     return 0;
